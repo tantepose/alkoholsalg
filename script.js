c7aa315d9fd63ada183fac6ee6a51d696b85f779
tantepose <doktoresel@gmail.com>
2022-01-27 13:27:03 +0100

    changed object creation method, fixed wierd objects

--- script.js
+++ script.js
@@ -2,53 +2,55 @@
 const { closed_dates, limited_dates } = require("./calendar.js")
 
 // define a day
-const dayPrototype = {
-    dateObject: new Date (),
-    get date () {
+function day () {
+    this.dateObject = new Date ()
+    
+    this.date = function () {
         return this.dateObject.getDate() + "/" + (this.dateObject.getMonth() + 1)
-    },
-    get day () {
+    }
+
+    this.day = function () {
         return this.dateObject.getDay()
-    },
-    status: undefined
+    }
+
+    this.status = undefined
 }
 
 // make today
-const today = Object.create(dayPrototype)
+const today = new day
 today.status = getStatus(today)
-console.log(today + "\n")
+console.log("🍷 Status for " + today.date() + ": " + today.status + "\n")
 
 // make tomorrow
-const tomorrow = Object.create(dayPrototype)
+const tomorrow = new day
 tomorrow.dateObject.setDate(tomorrow.dateObject.getDate() + 1) // make tomorrow actually tomorrow
 tomorrow.status = getStatus(tomorrow)
-console.log(tomorrow + "\n")
+console.log("🍷 Status for " + tomorrow.date() + ": " + tomorrow.status + "\n")
 
 // make day after tomorrow
-const day_after_tomorrow = Object.create(dayPrototype)
-day_after_tomorrow.dateObject.setDate(day_after_tomorrow.dateObject.getDate() + 2) // NOT WORKING: make day_after_tomorrow actually day_after_tomorrow
+const day_after_tomorrow = new day
+day_after_tomorrow.dateObject.setDate(day_after_tomorrow.dateObject.getDate() + 2) // make day_after_tomorrow actually day_after_tomorrow
 day_after_tomorrow.status = getStatus(day_after_tomorrow)
-console.log(day_after_tomorrow)
+console.log("🍷 Status for " + day_after_tomorrow.date() + ": " + day_after_tomorrow.status + "\n")
 
 // functions
 function getStatus (date) {
-    console.log("🔍 Checking " + date.date + " for abnormalities...")
+    console.log("🔍 Checking " + date.date() + " for abnormalities...")
 
-    if (closed_dates.indexOf(date.date) > -1) {
+    if (closed_dates.indexOf(date.date()) > -1) {
         console.log("🎯 Found date in closed_dates!")
         return "closed"
-    } else if (limited_dates.indexOf(date.date) > -1) {
+    } else if (limited_dates.indexOf(date.date()) > -1) {
         console.log("🎯 Found date in limited_dates!")
         return "limited"
-    } else if (date.day == 0) {
+    } else if (date.day() == 0) {
         console.log("🎯 Date not found, but is sunday!")
         return "closed"
-    } else if (date.day == 6) {
+    } else if (date.day() == 6) {
         console.log("🎯 Date not found, but is saturday!")
         return "limited"
     } else {
-        console.log("📂 Date not found, all normal!")
+        console.log("🎯 Date not found, all normal!")
         return "normal"
     }
 }
-
